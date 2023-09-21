47e999e9a3b8da8d230d0c4ffa2840ec7791a83a
XineAurora <93967850+XineAurora@users.noreply.github.com>
2023-09-21 19:44:47 +0400

    Create app.go

---
+++ intrernal/app/app.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/XineAurora/fio-statistics/intrernal/database"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// open db
+// run kafka consumer
+// run api
+
+type App struct {
+	repo database.FIORepository
+	api  *http.Server
+}
+
+func New() *App {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DBNAME"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_SSLMODE"),
+		os.Getenv("POSTGRES_TIMEZONE"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &App{
+		repo: database.NewDBFIORepository(db),
+	}
+}
+
+func (app *App) Run() {
+
+}
